4cb9c84efdf14f0e5b002ba4529e30ebf9561868
unknown <vasavivijjapu14@gmail.com>
2024-11-01 18:40:30 +0530

    this is first commit

---
+++ react-flow-app/src/Add.jsx
@@ -0,0 +1,4 @@
+function Add(num1, num2){
+    return num1+num2 
+}
+export default Add
---
+++ react-flow-app/src/App.jsx
@@ -0,0 +1,9 @@
+import Add from "./Add"
+
+
+function App() {
+ 
+let res= Add(10,30)
+console.log(res)
+}
+export default App
