a20c6a99d3daa72e4806187037efb6d2df3e4f1e
vijjapu-vasavi <vasavivijjapu14@gmail.com>
2024-11-02 12:28:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h1>modification done by others </h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
